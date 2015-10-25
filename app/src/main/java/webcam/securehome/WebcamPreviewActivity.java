0eca08acdd166967afb44be52ef6d01b95a0dc00
HauckA <alexander.hauck@stud.hslu.ch>
2015-10-25 18:58:43 +0100

    Webcam Preview

--- app/src/main/java/webcam/securehome/WebcamPreviewActivity.java
+++ app/src/main/java/webcam/securehome/WebcamPreviewActivity.java
@@ -1,15 +1,24 @@
 package webcam.securehome;
 
-import android.content.SharedPreferences;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import webcam.securehome.helper.CameraView;
 
 /**
  * This class show a preview of the Webcam. Here the user can start to upload files to the webserver.
  */
 public class WebcamPreviewActivity extends AppCompatActivity {
+
+    //Camera stuff
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
 
     //Sring declaration
     private String webcamID = null;
@@ -21,6 +30,9 @@ public class WebcamPreviewActivity extends AppCompatActivity {
     //Button declaration
     private Button btnStartBroadcast = null;
 
+    //ImagePreview
+    private ImageView imageView = null;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webcampreview_layout);
@@ -31,9 +43,25 @@ public class WebcamPreviewActivity extends AppCompatActivity {
         //Button generation
         this.btnStartBroadcast = (Button) findViewById(R.id.btnStartBroadcast);
 
+        //ImageView generation
+        this.imageView = (ImageView) findViewById(R.id.imageView);
+
         //Set description-text
         //TODO Read Webcam Description from Database
         this.txtWebcamDescription.setText(webcamDescription);
+
+        try{
+            mCamera = Camera.open();//you can use open(int) to use different cameras
+        } catch (Exception e){
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+        if(mCamera != null) {
+            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.frameLayout);
+            camera_view.addView(mCameraView);//add the SurfaceView to the ImageView
+        }
+
 
 
     };
